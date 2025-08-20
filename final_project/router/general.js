b73e96c008f9d5de1f85bda1694feab91e76f277
luccae11235 <luccaemmanuel@gmail.com>
2025-08-20 10:31:32 -0400

    Implement tasks 1-3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,22 +11,35 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  return res.send(JSON.stringify(books, null, 4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book){
+        return res.status(200).send(JSON.stringify(book, null, 4));
+    } else {
+        return res.status(404).json({message:"Book not found!"})
+    }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', function (req, res) {
+    const authorParam = req.params.author.toLowerCase();
+  
+    // books is an object keyed by "isbn-like" ids
+    const matches = Object.entries(books)
+      .filter(([_, book]) => book.author && book.author.toLowerCase() === authorParam)
+      .map(([isbn, book]) => ({ isbn, ...book }));
+  
+    if (matches.length === 0) {
+      return res.status(404).json({ message: "No books found for this author" });
+    }
+    return res.status(200).send(JSON.stringify(matches, null, 4));
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
